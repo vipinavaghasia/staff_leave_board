df52ca33aecfb002ecdd49022567915c81aea3b7
vipinavaghasia <54425293+vipinavaghasia@users.noreply.github.com>
2020-07-20 19:53:44 +1000

    Update navbar.php

--- view/navbar.php
+++ view/navbar.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>TAFE </title>
+    <title>Staff Leave Board </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
